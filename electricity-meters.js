6f0d729969662d8396eee2bfe27015d9b5069b0a
sp010412 <smajwede7gmail.com>
2022-01-20 16:54:23 +0200

    catch

--- electricity-meters.js
+++ electricity-meters.js
@@ -3,44 +3,71 @@ module.exports = function (pool) {
 
 	// list all the streets the we have on records
 	async function streets() {
-		const streets = await pool.query(`select * from street`);
-		return streets.rows;
+		try {
+			const streets = await pool.query(`select * from street`);
+			return streets.rows;
+
+		} catch (err) {
+			console.log(err)
+		}
 	}
 
 	// for a given street show all the meters and their balances
 	async function streetMeters(streetId) {
+		try {
+			const metersForEachStreet = await pool.query(`select id, street_number, street_id, balance from  electricity_meter where street_id = $1 order by id`, [streetId]);
+			return metersForEachStreet.rows;
 
-		const metersForEachStreet = await pool.query(`select id, street_number, street_id, balance from  electricity_meter where street_id = $1 order by id`, [streetId]);
-		return metersForEachStreet.rows;
+		} catch (err) {
+			console.log(err)
+		}
 	}
 
 	async function idMeters(id) {
-
-		const metersForEachStreet = await pool.query(`select id, street_number, street_id, balance from  electricity_meter where id = $1 `, [id]);
-		return metersForEachStreet.rows;
+		try {
+			const metersForEachStreet = await pool.query(`select id, street_number, street_id, balance from  electricity_meter where id = $1 `, [id]);
+			return metersForEachStreet.rows;
+		} catch (err) {
+			console.log(err)
+		}
 	}
 
 	// return all the appliances
 	async function appliances() {
-		const usagePerAppliance = await pool.query(`select * from appliance`);
-		return usagePerAppliance.rows;
+		try {
+			const usagePerAppliance = await pool.query(`select * from appliance`);
+			return usagePerAppliance.rows;
+		} catch (err) {
+			console.log(err)
+		}
 	}
 
 	// increase the meter balance for the meterId supplied
 	async function topupElectricity(meterId, units) {
-		await pool.query('UPDATE electricity_meter SET balance = balance + $1 WHERE id = $2', [units, meterId]);
+		try {
+			await pool.query('UPDATE electricity_meter SET balance = balance + $1 WHERE id = $2', [units, meterId]);
+		} catch (err) {
+			console.log(err)
+		}
 	}
 
 	// return the data for a given balance/id
 	async function meterData(meterId) {
-
-		const balance = await pool.query(`select balance from electricity_meter where id = $1 `, [meterId]);
-		return balance.rows;
+		try {
+			const balance = await pool.query(`select balance from electricity_meter where id = $1 `, [meterId]);
+			return balance.rows;
+		} catch (err) {
+			console.log(err)
+		}
 	}
 
 	// decrease the meter balance for the meterId supplied
 	async function useElectricity(meterId, units) {
-		await pool.query('UPDATE electricity_meter SET balance = balance - $1 WHERE id = $2', [units, meterId]);
+		try {
+			await pool.query('UPDATE electricity_meter SET balance = balance - $1 WHERE id = $2', [units, meterId]);
+		} catch (err) {
+			console.log(err)
+		}
 	}
 
 	return {
